7aec146a26a71314a76e97eb360e1e839ff9b3d0
David Payne <dpayne2841@live.com>
2019-11-03 14:33:47 -0600

    Added Instruction and Script Templates
    Just added our Instructions and Scripts templates to make life a little easier and to keep the same style throughout our addons.

---
+++ ScriptTemplate.cs
@@ -0,0 +1,48 @@
+// =======================================================================================
+// Created and maintained by iMMO
+// Usable for both personal and commercial projects, but no sharing or re-sale
+// * Discord Support Server.............: https://discord.gg/YkMbDHs
+// * Public downloads website...........: https://www.indie-mmo.net
+// * Pledge on Patreon for VIP AddOns...: https://www.patreon.com/IndieMMO
+// * Instructions.......................: https://indie-mmo.net/knowledge-base/
+// =======================================================================================
+using UnityEngine;
+
+// =======================================================================================
+// CHANGE ME
+// =======================================================================================
+public partial class ChangeMe : MonoBehaviour
+{
+    #region Variables
+
+	[Header("Components")]
+	public GameObject example;
+
+    #endregion
+
+    #region Unity Functions
+
+	// -----------------------------------------------------------------------------------
+    // Start
+	// Place a lovely description here.
+    // -----------------------------------------------------------------------------------
+	private void Start()
+	{
+		
+	}
+
+    #endregion
+
+    #region Custom Functions
+
+	// -----------------------------------------------------------------------------------
+    // Example
+	// Place a lovely description here.
+    // -----------------------------------------------------------------------------------
+	private void Example()
+	{
+		
+	}
+
+    #endregion
+}
